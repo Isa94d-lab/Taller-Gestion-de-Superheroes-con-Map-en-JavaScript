1e7c2c0e507ddd959c9071ee511c9d8aec84f4aa
Isa94d-lab <181164074+Isa94d-lab@users.noreply.github.com>
2025-01-13 19:39:37 -0500

    ✨ Primer commit
    Creacion del "MAPA"

---
+++ js/app.js
@@ -0,0 +1,46 @@
+/* Creamos el mapa que almacenera nuestros superheroes*/
+const superheroes = new Map ([
+    [
+    1, 
+    {
+        id: 1,
+        nombre: "Iron Man",
+        universo: "Marvel",
+        poder: "Millonario, Play Boy, Filantropo",
+        fuerza: 85
+      }], 
+    [
+    2, {
+        id: 2,
+        nombre: "Spiderman",
+        universo: "Marvel",
+        poder: "La araña  humana",
+        fuerza: 90
+      }],
+    [
+
+    3, {
+        id: 3,
+        nombre: "Deadpool",
+        universo: "Marvel",
+        poder: "Regeneracion",
+        fuerza: 100
+      }],
+    
+    [
+    4, {
+        id: 4,
+        nombre: "BatMan",
+        universo: "DC",
+        poder: "Millonario",
+        fuerza: 85
+      }],
+    [
+    5, {
+        id: 5,
+        nombre: "Flash",
+        universo: "DC",
+        poder: "Super velocidad",
+        fuerza: 80
+      }],
+])
