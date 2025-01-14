ab39fd47188d06e95356a52a1b69849842e4a5a6
Isa94d-lab <181164074+Isa94d-lab@users.noreply.github.com>
2025-01-13 21:23:40 -0500

    ✨ Segundo Commit
    Se crea una funcion que permite visualizar informacion del heroe escogido

--- js/app.js
+++ js/app.js
@@ -44,3 +44,28 @@ const superheroes = new Map ([
         fuerza: 80
       }],
 ])
+
+/* Le solicitamos al usuario que ingrese el numero del superheroese deceado */
+
+let super_escogido = prompt("De que superheroe te gustaria ver informacion? (INGRESA SOLO EL NUMERO) 1. IronMan / 2. Spiderman / 3. Deadpool / 4. BatMan / 5. Flash",
+);
+
+/* Para permitir q el numero ingresado por el usuario sea usado, es necesario pasarlo a Int ya que antes se encontraba en string*/
+let numero_super_escogido = parseInt(numeroString);
+
+/* Creamos una funcion encargada de mostrar la info del heroe dependiendo de la seleccion del usuario*/
+function buscarSuperheroePor(numero_super_escogido) {
+    /* Esto nos permitira extraer la informacion con la "llave" usando el numero del heroe escogido*/
+    let heroe = superheroes.get(numero_super_escogido);
+    if (heroe) {
+        console.log(`ID: ${heroe.id}`);
+        console.log(`Nombre: ${heroe.nombre}`);
+        console.log(`Universo: ${heroe.universo}`);
+        console.log(`Poder: ${heroe.poder}`);
+        console.log(`Fuerza: ${heroe.fuerza}`);
+          } else {
+            console.log("El superhéroe con ese número no existe.");
+          }
+        }
+
+buscarSuperheroePor(numero_super_escogido);
